de924fe8edcf80e2d77e56b5aca917362410438e
RiceMarch <1079966197@qq.com>
2021-04-06 21:32:28 +0800

    [feature] gateway init
    引入 spring cloud gateway

---
+++ cms-gateway/src/main/java/com/ricemarch/cmsgateway/CmsGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ricemarch.cmsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsGatewayApplication.class, args);
+    }
+
+}
---
+++ cms-gateway/src/test/java/com/ricemarch/cmsgateway/CmsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricemarch.cmsgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
